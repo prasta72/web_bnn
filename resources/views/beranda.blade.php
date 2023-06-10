cb778132c8da9d5b04b8316aeb891e97b5b6e1ee
Pradipta610 <pradipta.wistika@gmail.com>
2023-06-10 15:12:31 +0800

    fix home

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -8,17 +8,31 @@
     <title>Halaman Beranda BNN</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
 </head>
+<style>
+    .bg-home {
+        background: url("{{ asset('/img/kantor-bnn.jpg') }}") no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
 
-<body>
+    .section {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        width: 100%
+    }
+</style>
 
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+<body class="bg-home">
+
+    <nav class="bg-transparent border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" class="flex items-center">
-                <img src="{{ asset('/img/bnn.png') }}" class="h-8 mr-3" alt="Flowbite Logo" />
-                <span
-                    class=" md:block hidden self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kantor
-                    BNN Provinsi
-                    Bali</span>
+                <img src="{{ asset('/img/bnn.png') }}" class="h-20 mr-3" alt="Flowbite Logo" />
             </a>
             <button data-collapse-toggle="navbar-default" type="button"
                 class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -34,26 +48,26 @@
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 {{-- {{ dd(Auth::guard('web')->user()) }} --}}
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    class=" font-medium flex flex-col text-center mx-auto p-4 md:p-0 mt-4 md:border border-gray-100 rounded-full bg-transparent md:flex-row md:space-x-8 md:mt-0  md:bg-transparent  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     @if (Auth::guard('admins')->user() != null)
                         <li>
                             <a href="{{ route('adminDashboard') }}">
                                 <button type="button"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dashboard</button>
+                                    class="text-white border rounded-full transparent-button hover:bg-transparent focus:ring-4 focus:outline-none focus:ring-white font-medium text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-transparent dark:hover:bg-transparent dark:focus:ring-white hover:bg-white hover:text-gray-900">Dashboard</button>
                             </a>
                         </li>
                     @elseif(Auth::guard('web')->user() != null)
                         <li>
                             <a href="{{ route('userDashboard') }}">
                                 <button type="button"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dashboard</button>
+                                    class="text-white border rounded-full transparent-button hover:bg-transparent focus:ring-4 focus:outline-none focus:ring-white font-medium text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-transparent dark:hover:bg-transparent dark:focus:ring-white hover:bg-white hover:text-gray-900">Dashboard</button>
                             </a>
                         </li>
                     @elseif(Auth::guard('web')->user() == null || Auth::guard('admins')->user() == null)
                         <li>
                             <a href="{{ route('login') }}">
                                 <button type="button"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                                    class="text-white border rounded-full transparent-button hover:bg-transparent focus:ring-4 focus:outline-none focus:ring-white font-medium text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-transparent dark:hover:bg-transparent dark:focus:ring-white hover:bg-white hover:text-gray-900">Login</button>
                             </a>
                         </li>
                     @endif
@@ -62,34 +76,32 @@
             </div>
         </div>
     </nav>
-    <section class="p-4 md:p-20">
-        <div class="flex flex-col md:flex-row">
-            <div class="flex p-4 w-full md:w-1/2">
-                <img class="rounded-lg" src="{{ asset('/img/bnn-bali.jpg') }}" alt="">
-            </div>
-            <div class="flex p-4 w-full md:w-1/2">
-                <p class="text-2xl text-justify"> Selain tugas sebagaimana diatas, BNN juga bertugas menyusun dan
-                    melaksanakan kebijakan nasional mengenai pencegahan dan pemberantasan penyalahgunaan dan peredaran
-                    gelap psikotropika, prekursor dan bahan adiktif lainnya kecuali bahan adiktif untuk tembakau dan
-                    alkohol. </p>
-            </div>
-        </div>
-    </section>
-    @if (Auth::guard('admins')->user() != null)
-    @elseif(Auth::guard('web')->user() != null)
-    @elseif(Auth::guard('web')->user() == null || Auth::guard('admins')->user() == null)
-        <section class="p-4 md:p-20">
-            <div class="flex flex-col md:flex-row mx-auto my-0 justify-center text-center">
-                <button>
-                    <a href="{{ route('daftarUser') }}">
-                        <button type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pendaftaran</button>
-                    </a>
-                </button>
+    <div class="section">
+        <section class="p-4 md:p-20 ">
+            <div class="flex">
+                <div class="flex flex-col justify-center items-center text-white p-4 w-full md:w-1/2">
+                    <h5 class="text-5xl font-semibold">BNN Provinsi Bali</h5>
+                    <p class="text-xl text-justify"> Selamat Datang di Website Pengelolaan Kegiatan Kerja Praktek </p>
+                </div>
             </div>
         </section>
-    @endif
+        @if (Auth::guard('admins')->user() != null)
+        @elseif(Auth::guard('web')->user() != null)
 
+        @elseif(Auth::guard('web')->user() == null || Auth::guard('admins')->user() == null)
+            <section class="p-4 md:p-20">
+                <div class="flex flex-col md:flex-row mx-auto my-0 justify-center text-center">
+                    <button>
+                        <a href="{{ route('daftarUser') }}">
+                            <button type="button"
+                                class="text-white bg-transparent border rounded-full hover:bg-white hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-white font-medium text-sm px-8 py-2 text-center mr-3 md:mr-0 dark:bg-white dark:hover:bg-white dark:focus:ring-white">Daftar
+                                Kerja Praktek</button>
+                        </a>
+                    </button>
+                </div>
+            </section>
+        @endif
+    </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 
 </body>
