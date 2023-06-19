629e15710fb2b56bc96df23d689a4315bdea84cf
xarthaa <manager.arthachan@gmail.com>
2023-06-19 20:34:29 +0800

    Adding seeder to DatabaseSeeder.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\KerjaPraktek;
 use App\Models\Pembina;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,11 +12,11 @@ use Illuminate\Support\Facades\Auth;
 class AdminController extends Controller
 {
     public function index() {
-        $dataUser = User::all();
+        $dataUser = KerjaPraktek::all();
         $countUser = count($dataUser);
         $dataAdmin = Pembina::all();
         $countAdmin = count($dataAdmin);
         return view('pages.admin.adminDashboard', compact('dataUser', 'countUser', 'dataAdmin', 'countAdmin'));
     }
-    
+
 }
