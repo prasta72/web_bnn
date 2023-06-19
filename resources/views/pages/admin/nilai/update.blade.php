10b495c4e9d8368932874f1966cf4a734d4a2751
xarthaa <manager.arthachan@gmail.com>
2023-06-19 19:40:19 +0800

    revisi bidang kerja

--- resources/views/pages/admin/nilai/update.blade.php
+++ resources/views/pages/admin/nilai/update.blade.php
@@ -75,8 +75,8 @@
                                         @if ($data->kerjapraktek->pembina == null)
                                         value="Pembina Belum Ditentukan"
                                         @else
-                                        value="{{ 
-                                        
+                                        value="{{
+
                                             old('admin_id', $data->kerjapraktek->pembina->nama_pembina) }}"
                                         @endif
                                        >
@@ -90,7 +90,7 @@
                                     <select name="bidang_kerja" id="pembina"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         @foreach ($kerjapraktek as $item)
-                                            <option value="{{ $item->bidang_kerja}}">
+                                            <option value="{{ $item->bidang_kerja}}" @if($item->bidang_kerja == $data->kerjapraktek['bidang_kerja']) selected @endif>
                                                 {{ $item->bidang_kerja }}</option>
                                         @endforeach
                                     </select>
